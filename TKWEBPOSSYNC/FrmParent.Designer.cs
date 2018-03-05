17ba7ab14a6658681821fed9b9ca82b7045778d5
at3158 <tk290@tkfood.com.tw>
2018-03-05 14:30:07 +0800

    add

---
+++ TKWEBPOSSYNC/FrmParent.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace TKWEBPOSSYNC
+{
+    partial class FrmParent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmParent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmParent";
+            this.Text = "FrmParent";
+            this.Load += new System.EventHandler(this.FrmParent_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
